2d523f73c531791561b82b6c1cb64a5cfa274cab
Silas Boyd-Wickizer <sbw@kid.csail.mit.edu>
2009-09-07 12:53:59 -0400

    lab1

---
+++ grade-functions.sh
@@ -0,0 +1,58 @@
+verbose=false
+
+if [ "x$1" = "x-v" ]
+then
+	verbose=true
+	out=/dev/stdout
+	err=/dev/stderr
+else
+	out=/dev/null
+	err=/dev/null
+fi
+
+if gmake --version >/dev/null 2>&1; then make=gmake; else make=make; fi
+
+pts=5
+timeout=30
+preservefs=n
+qemu=`$make -s --no-print-directory which-qemu`
+
+echo_n () {
+	# suns can't echo -n, and Mac OS X can't echo "x\c"
+	# assume argument has no doublequotes
+	awk 'BEGIN { printf("'"$*"'"); }' </dev/null
+}
+
+run () {
+	# Find the address of the kernel readline function,
+	# which the kernel monitor uses to read commands interactively.
+	brkaddr=`grep 'readline$' obj/kern/kernel.sym | sed -e's/ .*$//g'`
+	#echo "brkaddr $brkaddr"
+
+	# Generate a unique GDB port
+	port=`expr $UID % 5000 + 25000`
+
+	# Run qemu, setting a breakpoint at readline(),
+	# and feeding in appropriate commands to run, then quit.
+	t0=`date +%s.%N 2>/dev/null`
+	(
+		ulimit -t $timeout
+		$qemu -nographic $qemuopts -serial null -parallel file:jos.out -no-kqemu -s -S -p $port
+	) >$out 2>$err &
+
+	(
+		echo "target remote localhost:$port"
+                if [ "x$qemuphys" = "x" ]; then
+		    echo "br *0x$brkaddr"
+                else
+		    echo "br *(0x$brkaddr-0xf0000000)"
+                fi
+		echo c
+	) > jos.in
+
+	gdb -batch -nx -x jos.in > /dev/null
+	t1=`date +%s.%N 2>/dev/null`
+	time=`echo "scale=1; ($t1-$t0)/1" | sed 's/.N/.0/g' | bc 2>/dev/null`
+	time="(${time}s)"
+	rm jos.in
+}
---
+++ grade-lab1.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+qemuopts="-hda obj/kern/kernel.img"
+qemuphys=1
+source grade-functions.sh
+
+
+$make
+run
+
+score=0
+
+	echo_n "Printf: "
+	if grep "6828 decimal is 15254 octal!" jos.out >/dev/null
+	then
+		score=`expr 20 + $score`
+		echo OK $time
+	else
+		echo WRONG $time
+	fi
+
+	echo_n "Backtrace: "
+	args=`grep "ebp f01.* eip f0100.* args" jos.out | awk '{ print $6 }'`
+	cnt=`echo $args | grep '^00000000 00000000 00000001 00000002 00000003 00000004 00000005' | wc -w`
+	if [ $cnt -eq 8 ]
+	then
+		score=`expr 10 + $score`
+		echo_n "Count OK"
+	else
+		echo_n "Count WRONG"
+	fi
+
+	cnt=`grep "ebp f01.* eip f0100.* args" jos.out | awk 'BEGIN { FS = ORS = " " }
+{ print $6 }
+END { printf("\n") }' | grep '^00000000 00000000 00000001 00000002 00000003 00000004 00000005' | wc -w`
+	if [ $cnt -eq 8 ]; then
+		score=`expr 10 + $score`
+		echo_n ', Args OK'
+	else
+		echo_n ', Args WRONG (' $args ')'
+	fi
+
+	syms=`grep "kern/init.c:.* test_backtrace" jos.out`
+	symcnt=`grep "kern/init.c:.* test_backtrace" jos.out | wc -l`
+	if [ $symcnt -eq 6 ]; then
+		score=`expr 10 + $score`
+		echo , Symbols OK $time
+	else
+		echo , Symbols WRONG "($syms)" $time
+	fi
+
+echo "Score: $score/50"
+
+if [ $score -lt 50 ]; then
+    exit 1
+fi
